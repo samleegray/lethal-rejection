a2ccbece6250bddf371bb54e4c004b266f3b8c5f
Sam Gray <samleegray@gmail.com>
2025-02-05 16:54:28 -0600

    Updates

---
+++ precache-manifest.d1b45e53f4b87e3645c2df6ee3cbde1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26362e05105b094645829fb1ee701716",
+    "url": "/git@github.com:samleegray/lethal-rejection.git/index.html"
+  },
+  {
+    "revision": "ef3264418af0ae5c0f53",
+    "url": "/git@github.com:samleegray/lethal-rejection.git/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1b000fa80a7020a63735",
+    "url": "/git@github.com:samleegray/lethal-rejection.git/static/js/2.2693f6d3.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/git@github.com:samleegray/lethal-rejection.git/static/js/2.2693f6d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d327c41c62fd3a6dd36d",
+    "url": "/git@github.com:samleegray/lethal-rejection.git/static/js/3.8d3ce625.chunk.js"
+  },
+  {
+    "revision": "ef3264418af0ae5c0f53",
+    "url": "/git@github.com:samleegray/lethal-rejection.git/static/js/main.1fa16fcf.chunk.js"
+  },
+  {
+    "revision": "611df6f0997b66820577",
+    "url": "/git@github.com:samleegray/lethal-rejection.git/static/js/runtime-main.b0775e77.js"
+  }
+]);
