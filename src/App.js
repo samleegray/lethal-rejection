43e59965c5c9859bc5d66c53d2c248619783cc52
Sam Gray <samleegray@gmail.com>
2025-02-05 17:05:22 -0600

    main updates

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <header>
-        <h1>Welcome to My Static Site!</h1>
+        <h1>Welcome to My Static Site! 1</h1>
       </header>
       <main>
         <p>This is a simple static React website.</p>
