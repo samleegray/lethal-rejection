575bf420170439de13e118c642b9ccd178c850f4
Sam Gray <samleegray@gmail.com>
2025-02-05 16:26:32 -0600

    basics

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Welcome to My Static Site!</h1>
+      </header>
+      <main>
+        <p>This is a simple static React website.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
