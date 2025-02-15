ff017d6a9089069ed99a38e4fea1fd5462cc8c1d
Sam Gray <samleegray@gmail.com>
2025-02-14 23:41:43 -0600

    Renamed to components
    This is what is used in tailwind.config.ts and thus needs to match so the CSS is available from daisyui.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
-import Navbar from "@/component/Navbar";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
