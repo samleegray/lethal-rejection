04d7182fff5947ae36450aa434998a6b0ca265c4
Sam Gray <samleegray@gmail.com>
2025-02-15 22:15:06 -0600

    Got tooltips working
    Yay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <button className="btn btn-primary m-2">Button</button>;
+  return (
+    <a href="https://eso-hub.com/en/skills/sorcerer/storm-calling/streak">
+      <img loading="lazy" decoding="async" className="eso-hub-skill-img" src="https://eso-hub.com/storage/skills/sorcerer/storm-calling/streak.png" alt="ESO-Hub Skill icon" height="50" width="50">
+      </img>
+    </a>
+  );
+  // return <button className="btn btn-primary m-2">Button</button>;
 }
